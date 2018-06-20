fb3697ce5085e7b15ad03586589871870c9bad66
vidyagopal <vidya2732@gmail.com>
2018-06-20 13:23:28 +0000

    Done.

--- currency.rb
+++ currency.rb
@@ -1,1 +1,13 @@
-# Write your cod here
+def usd_to_eur
+  usd = gets.chomp
+  eur = usd*0.86
+  puts eur
+end
+
+def eur_to_usd
+  eur = gets.chomp
+  usd = eur*1.16 
+  puts usd
+end
+usd_to_eur
+eur_to_usd
